f37514665e651d422d64f56dec36f7441e9f6fe1
AndreasHeindorff <heindorff94@gmail.com>
2017-10-29 16:25:37 +0100

    Added empty RestControllers

---
+++ Backend/src/main/java/api/Authentication.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Andreas
+ */
+
+@RestController
+public class Authentication {
+    
+}
